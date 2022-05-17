95d532b5b1956c4440804b1abb0c82037f9cfb5b
Falcon <>
2022-05-16 19:53:09 -0500

    codebase

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,6 +1,8 @@
 import "../styles/globals.css";
 import Link from "next/link";
+import React, { useEffect } from "react";
 
+function MyApp({ Component, pageProps }) {
   return (
     <div>
       <main>
