3c73e8bb592e2df63cef7cf4cb9ee6c5c906989a
Falcon <>
2022-05-17 18:50:37 -0500

    clase-16

--- solana-movies/programs/solana-movies/src/lib.rs
+++ solana-movies/programs/solana-movies/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("D2uXeWxmqYMFcdLiCQcJBRpMRfVV8eVH84jSxH5y8iD1");
 
 #[program]
 pub mod solana_movies {
